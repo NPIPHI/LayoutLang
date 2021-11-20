e35c0ae29ac3fd3c78a8a9958800714db8678033
Nicholas Phillips <nwp2@illinois.edu>
2021-11-19 22:38:24 -0600

    webpack

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,12 @@ const type_section = new TypeSection(funcs);
 const function_section = new FunctionSection(funcs);
 const export_section = new ExportSection(exports_arr);
 const code_section = new CodeSection(funcs);
-const wasm_output = new WasmOutput(type_section, function_section,export_section,code_section);
+const wasm_output = new WasmOutput(type_section, function_section, export_section, code_section);
 
 const output = wasm_output.encode();
 
+async function main(){
+    let {module, instance} = await WebAssembly.instantiate(output);
+    (globalThis as any).inst = instance;
+}
+main();
