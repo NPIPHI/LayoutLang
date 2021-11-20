da669491bec8c15978e7b56dc3dfbacfbeeb7bc1
Nicholas Phillips <nwp2@illinois.edu>
2021-11-20 00:38:53 -0600

    memory export

--- src/codegen.ts
+++ src/codegen.ts
@@ -17,19 +17,63 @@ export namespace T {
     export const u32 = new Uint8Array([0x7e]);
     export const f32 = new Uint8Array([0x7d]);
     export const f64 = new Uint8Array([0x7c]);
+    export const export_func = new Uint8Array([0x00]);
+    export const export_table = new Uint8Array([0x01]);
+    export const export_mem = new Uint8Array([0x02]);
+    export const export_global = new Uint8Array([0x03]);
+    export const limit_min = new Uint8Array([0x00]);
+    export const limit_min_max = new Uint8Array([0x01]);
 }
 
 namespace I {
     export const i32 = {
         const: new Uint8Array([0x41]),
+        load: new Uint8Array([0x28]),
+        store: new Uint8Array([0x36]),
+        eqz: new Uint8Array([0x45]),
+        eq: new Uint8Array([0x46]),
+        ne: new Uint8Array([0x47]),
+        lt_s: new Uint8Array([0x48]),
+        lt_u: new Uint8Array([0x49]),
+        gt_s: new Uint8Array([0x4A]),
+        gt_u: new Uint8Array([0x4B]),
+        le_s: new Uint8Array([0x4C]),
+        le_u: new Uint8Array([0x4D]),
+        ge_s: new Uint8Array([0x4E]),
+        ge_u: new Uint8Array([0x4F]),
+        clz: new Uint8Array([0x67]),
+        ctz: new Uint8Array([0x68]),
+        popcnt: new Uint8Array([0x69]),
+        add: new Uint8Array([0x6A]),
+        sub: new Uint8Array([0x6B]),
+        mul: new Uint8Array([0x6C]),
+        div_s: new Uint8Array([0x6D]),
+        div_u: new Uint8Array([0x6E]),
+        rem_s: new Uint8Array([0x6F]),
+        rem_u: new Uint8Array([0x70]),
+        and: new Uint8Array([0x71]),
+        or: new Uint8Array([0x72]),
+        xor: new Uint8Array([0x73]),
+        shl: new Uint8Array([0x74]),
+        shr_s: new Uint8Array([0x75]),
+        shr_u: new Uint8Array([0x76]),
+        rotl: new Uint8Array([0x77]),
+        rotr: new Uint8Array([0x78]),
     }
 
+    export const memory = {
+        grow: new Uint8Array([0x40, 0x00]),
+        size: new Uint8Array([0x3f, 0x00])
+    }
+
+    export const drop = new Uint8Array([0x1A]);
+    export const select = new Uint8Array([0x1B]);
     export const _return = new Uint8Array([0x0f]);
     export const end_func = new Uint8Array([0x0b]);
 }
 
 export type PrimitiveType = Uint8Array;
-export type ExportKind = 0;
+export type ExportKind = Uint8Array;
 function merge_buffers(buffers: Uint8Array[]): Uint8Array{
     const length = buffers.reduce((a,b)=>a + b.byteLength, 0);
     const merged = new Uint8Array(length);
@@ -43,107 +87,147 @@ function merge_buffers(buffers: Uint8Array[]): Uint8Array{
     return merged;
 }
 
+function make_lenth_encoding(buffers: Uint8Array[]): Uint8Array {
+    const length = buffers.reduce((a,b)=>a + b.byteLength, 0);
+    return merge_buffers([encodeInt32(length), ...buffers]);
+}
+
+function make_vec(buffers: Uint8Array[]): Uint8Array {
+    return merge_buffers([encodeInt32(buffers.length), ...buffers])
+}
+
+function make_sized_vec(buffers: Uint8Array[]): Uint8Array {
+    const length_buffers = buffers.reduce((a,b)=>a + b.byteLength, 0);
+    const count = encodeInt32(buffers.length);
+    const length = encodeInt32(length_buffers + count.length);
+    return merge_buffers([length, count, ...buffers])
+}
+
 export class Function {
-    constructor(public inputs: PrimitiveType[], public outputs: PrimitiveType[]){}
+    constructor(public name: string, public inputs: PrimitiveType[], public output: PrimitiveType | 0){}
 
     encodeType(): Uint8Array {
         const func_type = T.func;
         const num_inputs = encodeInt32(this.inputs.length);
-        const num_outputs = encodeInt32(this.outputs.length);
+        const num_outputs = encodeInt32((this.output != 0) ? 1 : 0);
+        const output = this.output || new Uint8Array(0);
 
-        return merge_buffers([func_type, num_inputs, ...this.inputs, num_outputs, ...this.outputs]);
+        return merge_buffers([func_type, num_inputs, ...this.inputs, num_outputs, output]);
     }
 
     encodeCode(): Uint8Array {
         const local_declarations = encodeInt32(0);
-        const line1 = I.i32.const;
-        const line2 = encodeInt32(42);
-        const line3 = I._return;
-        const end = I.end_func;
+        const lines = [
+            // I.i32.const,
+            // encodeInt32(43),
+            // I.i32.const,
+            // encodeInt32(10),
+            // I.memory.grow,
+            // I.drop,
+            I.memory.size,
+            I.end_func
+        ]
 
-        const data = merge_buffers([local_declarations, line1, line2, line3, end]);
-        const data_size = encodeInt32(data.length);
-
-        return merge_buffers([data_size, data]);
+        return make_lenth_encoding([local_declarations, ...lines]);
     }
 }
 
-export class TypeSection {
+class TypeSection {
     constructor(public functions: Function[]){}
 
     encode(): Uint8Array {
         const id = encodeInt32(1);
-        const num_types = encodeInt32(this.functions.length);
         const type_encodings = this.functions.map(f=>f.encodeType())
 
-        const data = merge_buffers([num_types, ...type_encodings]);
-        const data_size = encodeInt32(data.length);
-
-        return merge_buffers([id, data_size, data]);
+        return merge_buffers([id, make_sized_vec(type_encodings)]);
     }
 }
 
-export class FunctionSection {
+class FunctionSection {
     constructor(public functions: Function[]){}
 
     encode(): Uint8Array {
         const id = T.function_section;
-
-        const num_functions = encodeInt32(this.functions.length);
         const index_functions = this.functions.map((_,i)=>encodeInt32(i));
 
-        const data = merge_buffers([num_functions, ...index_functions]);
-        const data_size = encodeInt32(data.length);
-
-        return merge_buffers([id, data_size, data]);
+        return merge_buffers([id, make_sized_vec(index_functions)]);
     }
 }
 
-export class Export {
-    constructor(public name: string, public kind: ExportKind, public exportIndex: number){}
+class FunctionExport {
+    constructor(public func: Function, public exportIndex: number){}
 
+    encode(): Uint8Array {
+        const encoder = new TextEncoder();
+        const name = encoder.encode(this.func.name);
+        const name_size = encodeInt32(name.length);
+        const index = encodeInt32(this.exportIndex);
+
+        return merge_buffers([name_size, name, T.export_func, index]);
+    }
+}
+
+class MemoryExport {
+    constructor(public name: string){}
     encode(): Uint8Array {
         const encoder = new TextEncoder();
         const name = encoder.encode(this.name);
         const name_size = encodeInt32(name.length);
-        const kind = encodeInt32(this.kind);
-        const index = encodeInt32(this.exportIndex);
+        const index = encodeInt32(0);
 
-        return merge_buffers([name_size, name, kind, index]);
+        return merge_buffers([name_size, name, T.export_mem, index]);
     }
 }
 
-export class ExportSection {
-    constructor(public exports: Export[]){}
+class ExportSection {
+    constructor(public exports: (FunctionExport | MemoryExport)[]){}
 
     encode(): Uint8Array {
         const id = T.export_section;
-        const num_exports = encodeInt32(this.exports.length);
         const exports = this.exports.map(e=>e.encode());
-        const data = merge_buffers([num_exports, ...exports]);
-        const data_size = encodeInt32(data.length);
-
-        return merge_buffers([id, data_size, data]);
+    
+        return merge_buffers([id, make_sized_vec(exports)]);
     }
 }
 
-export class CodeSection {
+class CodeSection {
     constructor(public functions: Function[]){}
 
     encode(): Uint8Array {
         const id = T.code_section;
-        const num_funcs = encodeInt32(this.functions.length);
         const funcs = this.functions.map(f=>f.encodeCode());
         
-        const data = merge_buffers([num_funcs, ...funcs]);
-        const data_len = encodeInt32(data.length);
+        return merge_buffers([id, make_sized_vec(funcs)]);
+    }
+}
 
-        return merge_buffers([id, data_len, data]);
+class Memory {
+    constructor(public min: number, public max: number | undefined){}
+
+    encode(): Uint8Array {
+        if(this.max != undefined){
+            const min = encodeInt32(this.min);
+            const max = encodeInt32(this.max);
+            return merge_buffers([T.limit_min_max, min, max]);
+        } else {
+            const min = encodeInt32(this.min);
+            return merge_buffers([T.limit_min, min]);
+        }
+        
+    }
+}
+
+class MemorySection{
+    encode(): Uint8Array {
+        const id = T.memory_section;
+        const mem = new Memory(1, undefined).encode()
+
+        return merge_buffers([id, make_sized_vec([mem])]);
     }
 }
 
 export class WasmOutput {
-    constructor(public type_section: TypeSection, public function_section: FunctionSection, public export_section: ExportSection, public code_section: CodeSection){}
+    constructor(public funcs: Function[]){}
 
     encode(): Uint8Array {
         const encoder = new TextEncoder();
@@ -151,11 +235,12 @@ export class WasmOutput {
         const header = encoder.encode("\0asm");
         const binary_version = new Uint8Array(new Uint32Array([1]).buffer);
         
-        const type_data = this.type_section.encode();
-        const function_data = this.function_section.encode();
-        const export_data = this.export_section.encode();
-        const code_data = this.code_section.encode();
+        const type_data = new TypeSection(this.funcs).encode();
+        const function_data = new FunctionSection(this.funcs).encode();
+        const export_data = new ExportSection([...this.funcs.map((f,i)=>new FunctionExport(f, i)), new MemoryExport("memory")]).encode();
+        const code_data = new CodeSection(this.funcs).encode();
+        const memory_data = new MemorySection().encode();
 
-        return merge_buffers([header, binary_version, type_data, function_data, export_data, code_data]);
+        return merge_buffers([header, binary_version, type_data, function_data, memory_data, export_data, code_data]);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,19 @@
-import { WasmOutput, T, Function, CodeSection, ExportSection, TypeSection, FunctionSection, Export } from "./codegen";
+import { WasmOutput, T, Function} from "./codegen";
+import { decodeInt32 } from "./leb/leb";
 const funcs = [
-    new Function([], [T.i32])
+    new Function("main", [], T.i32),
+    new Function("main2", [], T.i32)
 ];
 
-const exports_arr = [
-    new Export("main", 0, 0)
-];
-
-const type_section = new TypeSection(funcs);
-const function_section = new FunctionSection(funcs);
-const export_section = new ExportSection(exports_arr);
-const code_section = new CodeSection(funcs);
-const wasm_output = new WasmOutput(type_section, function_section, export_section, code_section);
+const wasm_output = new WasmOutput(funcs);
 
 const output = wasm_output.encode();
 
 async function main(){
     let {module, instance} = await WebAssembly.instantiate(output);
+    console.log((instance.exports as any).main());
     (globalThis as any).inst = instance;
 }
 main();
+
+console.log(decodeInt32(new Uint8Array([0xc8,0x01]), 0))
